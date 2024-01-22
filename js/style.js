c38e184280266b1180d3eac6d63da222659362e4
catalinbroinas <catalinbroinas@yahoo.com>
2024-01-22 17:07:40 +0200

    Add Book constructor Function Book() to handle book properties

--- js/style.js
+++ js/style.js
@@ -1,7 +1,11 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(title, author, page, category, read) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.category = category;
+    this.read = read;
 }
 
 function addBookToLibrary() {
