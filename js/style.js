501e32cc13b0c64ac5a62847a9bec8609f88f9a3
catalinbroinas <catalinbroinas@yahoo.com>
2024-02-26 15:50:55 +0200

    Implement functionality for open and close modal

--- js/style.js
+++ js/style.js
@@ -1,85 +1,85 @@
-const MY_LIBRARY = [];
-const BOOK_SECT = document.querySelector('#books');
-const MODAL_BOX = document.querySelector('#add-book-modal');
-const NEW_BOOK_BTN = document.querySelector('#btn-new-book');
-const CLOSE_BTN = document.querySelector('#close-button');
-const FORM_BOX = document.querySelector('#form-new-book');
+// const MY_LIBRARY = [];
+// const BOOK_SECT = document.querySelector('#books');
+// const MODAL_BOX = document.querySelector('#add-book-modal');
+// const NEW_BOOK_BTN = document.querySelector('#btn-new-book');
+// const CLOSE_BTN = document.querySelector('#close-button');
+// const FORM_BOX = document.querySelector('#form-new-book');
 
-function Book(title, author, page, category, read) {
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.category = category;
-    this.read = read;
-}
+// function Book(title, author, page, category, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.page = page;
+//     this.category = category;
+//     this.read = read;
+// }
 
-function createCard({ title, author, category, page, read }) {
-    const card = document.createElement('div');
-    const titleCard = document.createElement('h3');
-    const subTitle = document.createElement('h4');
-    const textPage = document.createElement('p');
-    const textDesc = document.createElement('p');
-    const textRead = document.createElement('p');
-    const groupBtn = document.createElement('div');
-    const readBtn = document.createElement('button');
-    const removeBtn = document.createElement('button');
-    const readIcon = document.createElement('i');
-    const removeIcon = document.createElement('i');
+// function createCard({ title, author, category, page, read }) {
+//     const card = document.createElement('div');
+//     const titleCard = document.createElement('h3');
+//     const subTitle = document.createElement('h4');
+//     const textPage = document.createElement('p');
+//     const textDesc = document.createElement('p');
+//     const textRead = document.createElement('p');
+//     const groupBtn = document.createElement('div');
+//     const readBtn = document.createElement('button');
+//     const removeBtn = document.createElement('button');
+//     const readIcon = document.createElement('i');
+//     const removeIcon = document.createElement('i');
 
-    card.classList.add('card');
-    titleCard.classList.add('title');
-    subTitle.classList.add('sub-title');
-    textPage.classList.add('text');
-    textDesc.classList.add('text');
-    textRead.classList.add('text', 'status');
-    groupBtn.classList.add('group-btn');
-    readBtn.classList.add('btn', 'btn-status');
-    removeBtn.classList.add('btn', 'btn-remove');
-    readIcon.classList.add('mdi', 'me-2');
-    removeIcon.classList.add('mdi', 'mdi-delete', 'me-2');
+//     card.classList.add('card');
+//     titleCard.classList.add('title');
+//     subTitle.classList.add('sub-title');
+//     textPage.classList.add('text');
+//     textDesc.classList.add('text');
+//     textRead.classList.add('text', 'status');
+//     groupBtn.classList.add('group-btn');
+//     readBtn.classList.add('btn', 'btn-status');
+//     removeBtn.classList.add('btn', 'btn-remove');
+//     readIcon.classList.add('mdi', 'me-2');
+//     removeIcon.classList.add('mdi', 'mdi-delete', 'me-2');
 
-    readBtn.setAttribute('type', 'button');
-    removeBtn.setAttribute('type', 'button');
+//     readBtn.setAttribute('type', 'button');
+//     removeBtn.setAttribute('type', 'button');
 
-    read ? textRead.classList.add('read') : textRead.classList.add('not-read');
-    read ? readBtn.classList.add('read') : readBtn.classList.remove('read');
-    read ? readIcon.classList.add('mdi-check-circle') : readIcon.classList.add('mdi-clock-time-four');
-    read = read ? 'Read' : 'Not read';
+//     read ? textRead.classList.add('read') : textRead.classList.add('not-read');
+//     read ? readBtn.classList.add('read') : readBtn.classList.remove('read');
+//     read ? readIcon.classList.add('mdi-check-circle') : readIcon.classList.add('mdi-clock-time-four');
+//     read = read ? 'Read' : 'Not read';
 
-    titleCard.textContent = title;
-    subTitle.textContent = author;
-    textDesc.textContent = category;
-    textPage.textContent = page;
-    textRead.textContent = read;
-    readBtn.textContent = 'Read';
-    removeBtn.textContent = 'Remove';
+//     titleCard.textContent = title;
+//     subTitle.textContent = author;
+//     textDesc.textContent = category;
+//     textPage.textContent = page;
+//     textRead.textContent = read;
+//     readBtn.textContent = 'Read';
+//     removeBtn.textContent = 'Remove';
 
-    card.appendChild(titleCard);
-    card.appendChild(subTitle);
-    card.appendChild(textDesc);
-    card.appendChild(textPage);
-    card.appendChild(textRead);
-    card.appendChild(groupBtn);
-    groupBtn.appendChild(readBtn);
-    groupBtn.appendChild(removeBtn);
-    removeBtn.appendChild(removeIcon);
-    readBtn.appendChild(readIcon);
+//     card.appendChild(titleCard);
+//     card.appendChild(subTitle);
+//     card.appendChild(textDesc);
+//     card.appendChild(textPage);
+//     card.appendChild(textRead);
+//     card.appendChild(groupBtn);
+//     groupBtn.appendChild(readBtn);
+//     groupBtn.appendChild(removeBtn);
+//     removeBtn.appendChild(removeIcon);
+//     readBtn.appendChild(readIcon);
 
-    return card;
-}
+//     return card;
+// }
 
-function displayCards(library) {
-    BOOK_SECT.innerHTML = '';
-    library.forEach(book => {
-        const card = createCard(book);
-        BOOK_SECT.appendChild(card);
-    });
-}
+// function displayCards(library) {
+//     BOOK_SECT.innerHTML = '';
+//     library.forEach(book => {
+//         const card = createCard(book);
+//         BOOK_SECT.appendChild(card);
+//     });
+// }
 
-function addBookToLibrary(book) {
-    MY_LIBRARY.push(book);
-    createCard(book);
-}
+// function addBookToLibrary(book) {
+//     MY_LIBRARY.push(book);
+//     createCard(book);
+// }
 
 function addBook() {
     const bookTitle = document.querySelector('#book-title').value;
@@ -95,13 +95,13 @@ function addBook() {
     displayCards(MY_LIBRARY);
 }
 
-function displayModal(modal) {
-    modal.style.display = 'block';
-}
+// function displayModal(modal) {
+//     modal.style.display = 'block';
+// }
 
-function closeModal(modal) {
-    modal.style.display = 'none';
-}
+// function closeModal(modal) {
+//     modal.style.display = 'none';
+// }
 
 function libraryEmpty() {
     if (MY_LIBRARY.length === 0) {
@@ -161,75 +161,75 @@ BUTTONS.forEach((button) => {
     });
 });
 
-window.addEventListener('load', () => {
-    displayCards(MY_LIBRARY);
-    libraryEmpty();
-});
-window.addEventListener('click', (event) => {
-    setTimeout((modal) => {
-        if (event.target === modal) {
-            closeModal(modal)
-        }
-    }, 500, MODAL_BOX);
-});
+// window.addEventListener('load', () => {
+//     displayCards(MY_LIBRARY);
+//     libraryEmpty();
+// });
+// window.addEventListener('click', (event) => {
+//     setTimeout((modal) => {
+//         if (event.target === modal) {
+//             closeModal(modal)
+//         }
+//     }, 500, MODAL_BOX);
+// });
 
-NEW_BOOK_BTN.addEventListener('click', () => {
-    setTimeout((modal) => {
-        displayModal(modal);
-    }, 500, MODAL_BOX);
-});
-CLOSE_BTN.addEventListener('click', () => {
-    setTimeout((modal) => {
-        closeModal(modal);
-    }, 500, MODAL_BOX);
-});
+// NEW_BOOK_BTN.addEventListener('click', () => {
+//     setTimeout((modal) => {
+//         displayModal(modal);
+//     }, 500, MODAL_BOX);
+// });
+// CLOSE_BTN.addEventListener('click', () => {
+//     setTimeout((modal) => {
+//         closeModal(modal);
+//     }, 500, MODAL_BOX);
+// });
 
-FORM_BOX.addEventListener('submit', (event) => {
-    event.preventDefault();
-    setTimeout((modal) => {
-        addBook();
-        closeModal(modal);
-        libraryEmpty();
-    }, 500, MODAL_BOX);
-});
+// FORM_BOX.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     setTimeout((modal) => {
+//         addBook();
+//         closeModal(modal);
+//         libraryEmpty();
+//     }, 500, MODAL_BOX);
+// });
 
 // Add a click event to Books section to handle "Read" and "Remove" button events
-BOOK_SECT.addEventListener('click', (event) => {
-    const target = event.target;
+// BOOK_SECT.addEventListener('click', (event) => {
+//     const target = event.target;
 
-    // Check if the clicked element has the "btn-status" or "btn-remove" class
-    if (target.classList.contains('btn-status')) {
-        const card = target.closest('.card'); // Get the card element associated with the button
-        const index = Array.from(BOOK_SECT.children).indexOf(card); // Get index of the card
+//     // Check if the clicked element has the "btn-status" or "btn-remove" class
+//     if (target.classList.contains('btn-status')) {
+//         const card = target.closest('.card'); // Get the card element associated with the button
+//         const index = Array.from(BOOK_SECT.children).indexOf(card); // Get index of the card
 
-        // Add ripple effect
-        rippleEffect(target);
+//         // Add ripple effect
+//         rippleEffect(target);
 
-        setTimeout((library) => {
-            // Toggle the "read" status
-            library[index].read = !library[index].read;
+//         setTimeout((library) => {
+//             // Toggle the "read" status
+//             library[index].read = !library[index].read;
 
-            // Update Books section display
-            displayCards(library);
-        }, 500, MY_LIBRARY);
-    } else if (target.classList.contains('btn-remove')) {
-        const card = target.closest('.card');
-        const index = Array.from(BOOK_SECT.children).indexOf(card);
+//             // Update Books section display
+//             displayCards(library);
+//         }, 500, MY_LIBRARY);
+//     } else if (target.classList.contains('btn-remove')) {
+//         const card = target.closest('.card');
+//         const index = Array.from(BOOK_SECT.children).indexOf(card);
 
-        // Add ripple effect
-        rippleEffect(target);
+//         // Add ripple effect
+//         rippleEffect(target);
 
-        setTimeout((library) => {
-            // Remove the book
-            library.splice(index, 1);
+//         setTimeout((library) => {
+//             // Remove the book
+//             library.splice(index, 1);
 
-            // Update Books section display
-            displayCards(library);
-        }, 500, MY_LIBRARY);
-        // Display a message if library is empty
-        setTimeout(libraryEmpty, 1000);
-    }
-});
+//             // Update Books section display
+//             displayCards(library);
+//         }, 500, MY_LIBRARY);
+//         // Display a message if library is empty
+//         setTimeout(libraryEmpty, 1000);
+//     }
+// });
 
 class Book {
     constructor(title, author, page, category, read) {
@@ -330,11 +330,32 @@ class UI {
         })
     }
 
-    displayModal = (modal) => {
-        modal.style.display = 'block';
+    displayModal = () => {
+        this.modalBox.style.display = 'block';
     }
 
-    closeModal = (modal) => {
-        modal.style.display = 'none';
+    closeModal = () => {
+        this.modalBox.style.display = 'none';
     }
 }
+
+const library = new Library();
+const ui = new UI();
+
+ui.newBookButton.addEventListener('click', () => {
+    setTimeout(() => {
+        ui.displayModal()
+    }, 500);
+});
+ui.closeButton.addEventListener('click', () => {
+    setTimeout(() => {
+        ui.closeModal();
+    }, 500);
+});
+window.addEventListener('click', (event) => {
+    setTimeout((modal) => {
+        if (event.target === modal) {
+            ui.closeModal(modal)
+        }
+    }, 500, ui.modalBox);
+});
